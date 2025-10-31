2a01dd41e545cb3f4e1d91e9c9b52396e55b3e33
Deepak Rana <ks970825@gmai;.com>
2025-10-31 15:17:37 +0530

    my first repo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Deepak Rana";
+        student.rollno = 8;
+        System.out.println(student);
+       student.walk();
+
+
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Student {String name;
+    int rollno;
+    Student(){}
+    Student(String name,int rollno ){
+        this.name=name;
+        this.rollno=rollno;
+    }
+    Dog(String name,)
+    public void walk() {
+        System.out.println("I can walk");
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollno=" + rollno +
+                '}';
+
+
+    }
+}
